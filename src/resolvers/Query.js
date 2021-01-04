e2685e5b164eb97c0fe92184bd6c1eecc58bf6e0
Brandon <brandon.dlamini95@gmail.com>
2021-01-04 18:38:40 +0200

    configured projects and defined Schema

---
+++ src/resolvers/Query.js
@@ -0,0 +1,26 @@
+import * as dynamodbLib from '../../libs/dynamodb-lib';
+
+export const test = (args,context) => {
+    return "your graphql Server is live ";
+};
+
+export const getAllSneakers = async (args, context) => {
+    const params ={
+        TableName: process.env.tableName
+    };
+
+    try {
+        const result = await dynamodbLib.call("scan", params);
+        return result.Items;
+
+    }
+
+    catch (e){
+        console.log(e)
+    }
+};
+
+export const getSneakersbyBrand = async (args, context) => {
+    
+}
+const 
