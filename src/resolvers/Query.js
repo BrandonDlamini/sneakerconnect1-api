a908a0a34a6ab4790414f87ba66d6ec6e9c4cd66
Brandon <brandon.dlamini95@gmail.com>
2021-01-04 20:44:49 +0200

    updated Query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,21 +6,23 @@ export const test = (args,context) => {
 
 export const getAllSneakers = async (args, context) => {
     const params ={
-        TableName: process.env.tableName
+        TableName: process.env.tableName,
+        Key
     };
 
     try {
         const result = await dynamodbLib.call("scan", params);
         return result.Items;
-
     }
 
     catch (e){
-        console.log(e)
+        console.log(e);
     }
 };
 
 export const getSneakersbyBrand = async (args, context) => {
-    
+    const params = {
+        TableName: process.env.tableName
+    }
 }
 const 
